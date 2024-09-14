d24c742eb1031a18b9b8f98747e5952392c6aefe
bilaljoyia <126891058+bilaljoyia@users.noreply.github.com>
2024-09-15 00:26:37 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Hero from './components/Home/Hero';
-import CreatPost from './components/Dashbord/CreatPost';
-import Resize from './components/Resize/Resize';
-import PdfConverter from './components/pdfconverter/PdfConverter';
-import JpegCompressor from './components/jpegcompressor/JpegCompressor';
-import PngCompressor from './components/pngcompressor/PngCompressor';
-import GifCompressor from './components/gifcompressor/GifCompressor';
-
+import Hero from "./components/Home/Hero";
+import CreatPost from "./components/Dashbord/CreatPost";
+import Resize from "./components/Resize/Resize";
+import PdfConverter from "./components/pdfconverter/PdfConverter";
+import JpegCompressor from "./components/jpegcompressor/JpegCompressor";
+import PngCompressor from "./components/pngcompressor/PngCompressor";
+import GifCompressor from "./components/gifcompressor/GifCompressor";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Hero />}/>
-        <Route path='/dashbor' element={<CreatPost/>}/>
-        <Route path='/resize' element={<Resize/>}/>
-        <Route path='/pdf-converter' element={<PdfConverter/>}/>
-        <Route path='/jpeg-image-compressor' element={<JpegCompressor/>}/>
-        <Route path='/png-image-compressor' element={<PngCompressor/>}/>
-        <Route path='/gif-image-compressor' element={<GifCompressor/>}/>
+        <Route path="/" element={<Hero />} />
+        <Route path="/dashboard" element={<CreatPost />} />
+        <Route path="/resize" element={<Resize />} />
+        <Route path="/pdf-converter" element={<PdfConverter />} />
+        <Route path="/jpeg-image-compressor" element={<JpegCompressor />} />
+        <Route path="/png-image-compressor" element={<PngCompressor />} />
+        <Route path="/gif-image-compressor" element={<GifCompressor />} />
       </Routes>
       <Footer />
     </Router>
